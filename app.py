b6c7cef5628df0d5e4cc41a2d7d616ace34cbfc4
hemanthrayudu624-bit <hemanthrayudu624@gmail.com>
2026-06-30 10:58:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import streamlit as st
 import os
 from PyPDF2 import PdfReader
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
